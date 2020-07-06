a4e58c9fc1179e0dcea3d9d6c3a62795d7bf6fd8
yahm23 <yahmed1996a@gmail.com>
2020-07-06 12:16:51 +0100

    added font successfully

--- src/components/structural/navbar.js
+++ src/components/structural/navbar.js
@@ -9,14 +9,14 @@ const NavbarTop = () => {
     return (
         <div>
             <div className="entireNavbar">
-                    <Link to="/"><img className="logo" height="150px" src={Logo}></img></Link>
+                    <Link to="/"><img className="logo" height="120px" src={Logo}></img></Link>
                 
-                    <Navbar className = "" expand="lg" variant="light" bg="light">
-                        <Navbar.Brand href="/">Home</Navbar.Brand>
-                        <Navbar.Brand href="/buying">Buying</Navbar.Brand>
-                        <Navbar.Brand href="/renting">Renting</Navbar.Brand>
-                        {/* <Navbar.Brand href="/selling">Selling</Navbar.Brand> */}
-                        <Navbar.Brand href="/contact">Contact</Navbar.Brand>
+                    <Navbar className = "navbar" expand="lg" variant="light" bg="light">
+                        <NavbarBrand href="/">Home</NavbarBrand>
+                        <NavbarBrand href="/buying">Buying</NavbarBrand>
+                        <NavbarBrand href="/renting">Renting</NavbarBrand>
+                        {/* <NavbarBrand href="/selling">Selling</NavbarBrand> */}
+                        <NavbarBrand href="/contact">Contact</NavbarBrand>
                         
                     </Navbar>
             </div>
