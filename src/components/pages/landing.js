728f82f490651a04c06c398a3c1e09e2f55e0aa6
yahm23 <yahmed1996a@gmail.com>
2020-07-06 16:48:18 +0100

    animation on scroll zoomy working

--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import IphoneScreen from '../media/phoneTemp.png'
 import Chevron from '../media/chevvy.svg'
+import AOS from 'aos'
 
 
 const Landing = () => {
+    AOS.init();
     return (
         <div>
             <div className = "landingPageContent">
@@ -12,13 +14,17 @@ const Landing = () => {
                         <h1 className="clipping bannerTitle" >control </h1>
                     <h1 className="bannerTitle" >of your energy use</h1>
                     <h2 className="bannerBlurb">Installed in your electric box, Jellyfish can save you money by accurately monitioring your energy use</h2>
-                    <div class="arrow bounce">
+                    <div className="arrow bounce">
                         <a  href="#preview"><img width="100px" src={Chevron}></img></a>
                     </div>
                 
                 </div>
                 <div id="preview" className="phonePreviewContainer">
                     <img className="iphoneScreen"  src={IphoneScreen}></img>
+                    {/* <div data-aos-anchor=".phonePreviewContainer"  data-aos-anchor-placement="top-top" data-aos="anim1"> */}
+                    <div data-aos="zoom-in">
+                        <h1 >this working</h1>
+                    </div>
                 </div>
             </div>
         </div>
