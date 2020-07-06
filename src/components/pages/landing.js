c3cba347a15a8b673d6033f822187dc6f89755da
yahm23 <yahmed1996a@gmail.com>
2020-07-06 12:28:00 +0100

    landing page fleshed out more with fon size/weight and positioning

--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Landing = () => {
     return (
         <div>
-            <h1 className="bannerTitle" >MHUNCHO</h1>
+            <h1 className="bannerTitle" >Take control of your energy use</h1>
         </div>
     )
 }
