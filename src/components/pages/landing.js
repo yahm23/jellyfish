268c9af34e673d7cfcb9a608ee2b9cc90f521536
yahm23 <yahmed1996a@gmail.com>
2020-07-07 09:44:20 +0100

    added all threee lorem features, with anim arrow

--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -15,7 +15,7 @@ const Landing = () => {
                     <h1 className="bannerTitle" >of your energy use</h1>
                     <h2 className="bannerBlurb">Installed in your electric box, Jellyfish can save you money by accurately monitioring your energy use</h2>
                     <div className="arrow bounce">
-                        <a  href="#preview"><img width="100px" src={Chevron}></img></a>
+                        <a  href="#preview"><img width="80px" src={Chevron}></img></a>
                     </div>
                 
                 </div>
@@ -26,6 +26,15 @@ const Landing = () => {
                         <h1 className="animArrow">←</h1>
                     </div>
                     <h1 className="feature">Feature: bla bla monitor real time accuractely</h1>
+                    <div data-aos="zoom-in" data-aos-duration="1000">
+                        <h1 className="animArrow">←</h1>
+                    </div>
+                    <h1 className="feature">Keep track of your highest energy used devices</h1>
+                    <div data-aos="zoom-in" data-aos-duration="1000">
+                        <h1 className="animArrow">←</h1>
+                    </div>
+                    <h1 className="feature">Reduce your energy usage by tracking total</h1>
+
                 </div>
             </div>
         </div>
