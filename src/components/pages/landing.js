23c8614f6482b47dfd8f2746e8e2ab92a209b9d9
yahm23 <yahmed1996a@gmail.com>
2020-07-07 11:35:16 +0100

    restructuring and new section added

--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -13,6 +13,14 @@ const Landing = () => {
             </div>
         )
     }
+
+    const AnimatedArrow = () => {
+        return(
+        <div className="animArrow" data-aos="zoom-in" data-aos-duration="4000">
+            <h1 >←</h1>
+        </div>
+    )
+    }
     return (
         <div>
             <div className = "landingPageContent">
@@ -21,24 +29,22 @@ const Landing = () => {
                         <h1 className="clipping bannerTitle" >control </h1>
                     <h1 className="bannerTitle" >of your energy use</h1>
                     <h2 className="bannerBlurb">Installed in your electric box, Jellyfish can save you money by accurately monitioring your energy use</h2>
-                    {ArrowBounce('#preview')}
+                    {ArrowBounce('#section1')}
                 </div>
-                <div id="preview" className="phonePreviewContainer">
-                    <img className="iphoneScreen"  src={IphoneScreen}></img>
-                    {/* <div data-aos-anchor=".phonePreviewContainer"  data-aos-anchor-placement="top-top" data-aos="anim1"> */}
-                    <div data-aos="zoom-in" data-aos-duration="1000">
-                        <h1 className="animArrow">←</h1>
+                <div id="section1" >
+                    <div  className="phonePreviewContainer">
+                        <img className="iphoneScreen"  src={IphoneScreen}></img>
+                        <AnimatedArrow></AnimatedArrow>
+                        <h1 className="feature">Feature: bla bla monitor real time accuractely</h1>
+                        <AnimatedArrow></AnimatedArrow>
+                        <h1 className="feature">Keep track of your highest energy used devices</h1>
+                        <AnimatedArrow></AnimatedArrow>
+                        <h1 className="feature">Reduce your energy usage by tracking total</h1>
                     </div>
-                    <h1 className="feature">Feature: bla bla monitor real time accuractely</h1>
-                    <div data-aos="zoom-in" data-aos-duration="1000">
-                        <h1 className="animArrow">←</h1>
-                    </div>
-                    <h1 className="feature">Keep track of your highest energy used devices</h1>
-                    <div data-aos="zoom-in" data-aos-duration="1000">
-                        <h1 className="animArrow">←</h1>
-                    </div>
-                    <h1 className="feature">Reduce your energy usage by tracking total</h1>
-
+                    {ArrowBounce('#section2')}
+                </div>
+                <div id="section2">
+                    <h2 className="subheading">Features</h2>
                 </div>
             </div>
         </div>
