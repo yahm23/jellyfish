078dc3dd69e3745001be2cf54400545757ad8ddb
yahm23 <yahmed1996a@gmail.com>
2020-07-28 16:38:27 +0100

    added new image on product page

--- src/components/pages/product.js
+++ src/components/pages/product.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import HandPhone from '../media/phoneApp-jelly.png'
+import PhoneScreen from '../media/jellyfishWithIphone.png'
 
 export default function Product() {
     console.log('hit the product page');
     return (
-        <div>
-            <h1>PRODUCT PAGE</h1>
+        <div className="productPage">
+            <div className="productBanner">
+                <img className="handImg" src={HandPhone} width="35%"/>
+                <h1 className="productBannerText">Set personalised energy usage limits to save energy & money.</h1>
+            </div>
+            <div className="productBanner">
+                <h1 className="productBannerText">Ask Jellyfish any questions</h1>
+                <img className="handImg" src={PhoneScreen} width="25%"/>
+            </div>
         </div>
     )
 }
