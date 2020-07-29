dbd96cfb236d5b533a8f3d7c8e1a3ac78bb186db
yahm23 <yahmed1996a@gmail.com>
2020-07-29 14:20:16 +0100

    made mock phone with comps from pure HTML

--- src/components/pages/technology.js
+++ src/components/pages/technology.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import ReactVivus from 'react-vivus';
+import Test from '../media/jelly.svg'
 
 export default function Technology() {
     return (
         <div className= "productPage">
-            <h1>Technology</h1>
-            
+            <h1 className="productBannerText">Technology</h1>
+           <div className = "iphoneMock" >
+                <div className = "screen"></div>
+                <div className ="homeButton"></div>
+           </div>
         </div>
     )
 }
