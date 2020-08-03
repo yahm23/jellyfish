88e686613b89e12c2daf3271cfc1f6e4ad48187a
yahm23 <yahmed1996a@gmail.com>
2020-08-03 16:55:39 +0100

    finally got an infitite graph loop, removed drawing animation, instead just cycles through

--- src/components/structural/animatedFeatures/graph.js
+++ src/components/structural/animatedFeatures/graph.js
@@ -5,11 +5,13 @@ export default function Graph() {
         <div>
             <svg  viewBox="0 0 315 107" version="1.1">
                 <defs>
+                    
                     <linearGradient x1="0%" y1="100%" x2="100%" y2="100%" id="linearGradient-1">
                         <stop stopColor="#ADD476" offset="0%"></stop>
                         <stop stopColor="#E7286C" offset="25%"></stop>
                         <stop stopColor="#ADD476" offset="90%"></stop>
                     </linearGradient>
+
                 </defs>
                 <g viewBox="0 0 315 107" id="grid">
                     <path className="horizontal" d="M.8.8H314.71"/>
@@ -37,7 +39,7 @@ export default function Graph() {
                     <path className="horizontal" d="M314.71,105.32V.8"/>
                 </g>
                 <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" sketch-type="MSPage">
-                    <path d="M2.10546875,95.75 L40.5546875,68.3476562 L55.2109375,81.1796875 L65.2148437,76.3945312 L96.1835937,86.8320312 L131.023438,19.9414062 L142.15625,23.7226562 L183.605469,2.1953125 L211.007812,22.3320312 L234.320312,71.5664062 L234.667969,83.0039062 L244.019531,83.0039062 L247.105469,88.8320312 L312.695312,104.839844" id="Path-1" stroke="#E7286C"  strokeWidth="4" sketch-type="MSShapeGroup" className="path"></path>
+                    <path  repeatCount="indefinite" d="M.21,99C12,89.83,24,81.83,37,72.83a3.67,3.67,0,0,1,3,1c5,6,13,14,20,7,11,1,21,8,32,9,13-21,21-43,34-64,4,0,8,3,12,2,14-6,26-14,39-20,10,7,21,14,29,23,7,19,20,36,22,56,4,0,10,0,11,5,20,6,41,11,62,16A106.24,106.24,0,0,0,315.51,98l1.08-.8C328,88.83,340,81.83,350,72.83c6,2,9,9,15,12,4,1,7-5,11-4,3,0,5,1,8,2,1,0,2,2,3,2,3,1,6,1,9,3a9,9,0,0,0,5,1,7.58,7.58,0,0,0,4,2c5-6,6-13,10-20,9-15,15-30,24-45,1-1,4,1,6,1,1,1,2,3,5,2,14-6,27-14,41-21,8,9,22,12,27,23,8,15,16,29,22,45,1,3,0,7,0,11,5-1,12-1,13,6,21,4,42,12,63,15,5-4,12.21-8.69,16.95-12.7" id="Path-1" stroke="#E7286C"  strokeWidth="4" sketch-type="MSShapeGroup" className="path"></path>
                 </g>
             </svg>
         </div>
