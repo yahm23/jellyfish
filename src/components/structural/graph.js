156d05d2c96c65232e75c2b4cc6e7f3ee9985242
yahm23 <yahmed1996a@gmail.com>
2020-08-03 12:09:04 +0100

    able to now add animated graph infinite

---
+++ src/components/structural/graph.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Graph() {
+    return (
+        <div>
+            <svg  viewBox="0 0 315 107" version="1.1">
+                <defs>
+                    <linearGradient x1="0%" y1="100%" x2="100%" y2="100%" id="linearGradient-1">
+                        <stop stop-color="#ADD476" offset="0%"></stop>
+                        <stop stop-color="#E7286C" offset="25%"></stop>
+                        <stop stop-color="#ADD476" offset="90%"></stop>
+                    </linearGradient>
+                </defs>
+                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch-type="MSPage">
+                    <path d="M2.10546875,95.75 L40.5546875,68.3476562 L55.2109375,81.1796875 L65.2148437,76.3945312 L96.1835937,86.8320312 L131.023438,19.9414062 L142.15625,23.7226562 L183.605469,2.1953125 L211.007812,22.3320312 L234.320312,71.5664062 L234.667969,83.0039062 L244.019531,83.0039062 L247.105469,88.8320312 L312.695312,104.839844" id="Path-1" stroke="url(#linearGradient-1)" stroke-width="4" sketch-type="MSShapeGroup" class="path"></path>
+                </g>
+            </svg>
+        </div>
+    )
+}
