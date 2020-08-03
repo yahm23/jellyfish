0d1b7b82a7b463a4c6efcefa8efe20d348a5ec6e
yahm23 <yahmed1996a@gmail.com>
2020-08-03 12:38:11 +0100

    graph grid correctly  scaled to line graph

--- src/components/structural/graph.js
+++ src/components/structural/graph.js
@@ -11,6 +11,31 @@ export default function Graph() {
                         <stop stop-color="#ADD476" offset="90%"></stop>
                     </linearGradient>
                 </defs>
+                <g viewBox="0 0 315 107" id="grid">
+                    <path class="horizontal" d="M.8.8H314.71"/>
+                    <path class="horizontal" d="M.8,15.65H314.71"/>
+                    <path class="horizontal" d="M.8,30.51H314.71"/>
+                    <path class="horizontal" d="M.8,45.36H314.71"/>
+                    <path class="horizontal" d="M.8,60.22H314.71"/>
+                    <path class="horizontal" d="M.8,75.07H314.71"/>
+                    <path class="horizontal" d="M.8,89.93H314.71"/>
+                    <path class="horizontal" d="M.8,104.78H314.71"/>
+                    <path class="horizontal" d="M.8,105.32V.8"/>
+                    <path class="horizontal" d="M23.22,105.32V.8"/>
+                    <path class="horizontal" d="M45.64,105.32V.8"/>
+                    <path class="horizontal" d="M68.07,105.32V.8"/>
+                    <path class="horizontal" d="M90.49,105.32V.8"/>
+                    <path class="horizontal" d="M112.91,105.32V.8"/>
+                    <path class="horizontal" d="M135.33,105.32V.8"/>
+                    <path class="horizontal" d="M157.75,105.32V.8"/>
+                    <path class="horizontal" d="M180.18,105.32V.8"/>
+                    <path class="horizontal" d="M202.6,105.32V.8"/>
+                    <path class="horizontal" d="M225,105.32V.8"/>
+                    <path class="horizontal" d="M247.44,105.32V.8"/>
+                    <path class="horizontal" d="M269.87,105.32V.8"/>
+                    <path class="horizontal" d="M292.29,105.32V.8"/>
+                    <path class="horizontal" d="M314.71,105.32V.8"/>
+                </g>
                 <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch-type="MSPage">
                     <path d="M2.10546875,95.75 L40.5546875,68.3476562 L55.2109375,81.1796875 L65.2148437,76.3945312 L96.1835937,86.8320312 L131.023438,19.9414062 L142.15625,23.7226562 L183.605469,2.1953125 L211.007812,22.3320312 L234.320312,71.5664062 L234.667969,83.0039062 L244.019531,83.0039062 L247.105469,88.8320312 L312.695312,104.839844" id="Path-1" stroke="url(#linearGradient-1)" stroke-width="4" sketch-type="MSShapeGroup" class="path"></path>
                 </g>
