f6616865a6907a04905ad5df8da7f6ca25cef143
yahm23 <yahmed1996a@gmail.com>
2020-08-05 09:25:16 +0100

    raw burger added  no funcitonaility yet but will squeeze accordingly

--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -209,6 +209,7 @@ const Landing = () => {
     }
     return (
         <div>
+            
             {isMobile? 
             <MobileLanding></MobileLanding> :
             <NormalLanding></NormalLanding>}
--- src/components/structural/navbar.js
+++ src/components/structural/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Modal from 'react-bootstrap/Modal'
 import NavbarBrand from 'react-bootstrap/NavbarBrand'
@@ -8,19 +8,32 @@ import boiger from '../media/Hamburger_icon.svg'
 import { useMediaQuery } from 'react-responsive'
 
 const NavbarTop = () => {
+    const[hamburgerActive,toggle] = useState(false)
+
     let isMobile = useMediaQuery({ maxWidth: 767 });
+
+    const clickHandler = () =>{
+        toggle(!hamburgerActive)
+
+    }
+
+
     return (
         <div>{ isMobile? 
             <div className="entireNavbar">
                 <Link to="/"><img className="logo" src={Logo}></img></Link>
-                <img className="logo" src={boiger}></img>
+                <button onClick={clickHandler} className={  "hamburger hamburger--squeeze "  +(hamburgerActive? "is-active":"")} type="button">
+                    <span className="hamburger-box">
+                        <span className="hamburger-inner"></span>
+                    </span>
+                </button>
             </div>
             
             :
             <div className="entireNavbar">
                     <Link to="/"><img className="logo" src={Logo}></img></Link>
                 
-                    <Navbar className = "navbar" expand="lg" variant="light" bg="light">
+                        <Navbar className = "navbar" expand="lg" variant="light" bg="light">
                         <NavbarBrand href="/products">Products</NavbarBrand>
                         <NavbarBrand href="/technology">Technology</NavbarBrand>
                         <NavbarBrand href="/support">Support</NavbarBrand>
