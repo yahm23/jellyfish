6a4a5a508ac27cbff26f980a9e73c9f46169b778
yahm23 <yahmed1996a@gmail.com>
2020-08-07 09:54:31 +0100

    Fixed font weight

--- src/components/pages/buy.js
+++ src/components/pages/buy.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Device from '../media/animationMedia/device.png'
 export default function Buy() {
     return (
         <div  className="rawPage">
-            <h2>we are in the buy page</h2>
+            <div className="buyPage">
+                    <h1 style={{textAlign:"center"}}className="productBannerText">Get Jellyfish.</h1>
+                    <div className="buyingGrid">
+                        <div >
+                            <img width="100%" src={Device} />
+                            <h1></h1>
+                        </div>
+                        
+                        <div className="buyDescription">
+                            <h1>Jellyfish</h1>
+                            <h4>Monitor your home’s energy use. Our base model, for non-Solar homes.</h4>
+                            <h5 className="buyDescription">Monitor your home’s energy use. This is our base model, with full functioanlity described on the site*</h5>
+                        
+                        </div>
+                        
+                        
+                    </div>
+            </div>
         </div>
     )
 }
